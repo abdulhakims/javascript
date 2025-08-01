ac25e05621a1ce0554adcfb1bf12c646c1a94af2
CL1-PC34 <CL1-PC34@DESKTOP-C7R1J9O>
2025-08-01 10:21:36 +0800

    sample program act2b

---
+++ src/main/java/com/mycompany/abdul/Abdul.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.abdul;
+
+/**
+ *
+ * @author CL1-PC34
+ */
+public class Abdul {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
